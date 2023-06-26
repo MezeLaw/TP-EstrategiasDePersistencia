d2d39e07f8bd891c3749fc73e29e4bf4715050cd
ncoluc <nicolasmcoluccio@gmail.com>
2023-06-26 20:43:50 -0300

    ajuste paths endpoint en español

--- routes/activityLogs.js
+++ routes/activityLogs.js
@@ -4,7 +4,7 @@ const jwtMiddleware = require('../jwt');
 const router = express.Router();
 
 
-// Get users
+// Obtener todos los logs
 router.get('/', async (req, res) => {
     try {
         const activities = await activityService.getActivities()
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
     }
 });
 
-//Get by id
+// Obtener por ID de usuario
 router.get("/:id_usuario", async (req, res) => {
     const userId = req.params.id_usuario;
     try {
@@ -31,21 +31,6 @@ router.get("/:id_usuario", async (req, res) => {
 });
 
 
-router.get("/date/:exact_date", async (req, res) => {
-    const exactDate = new Date(req.params.exact_date);
-    try {
-      jwtMiddleware.verifyAndParseToken(req);
-  
-      const activities = await activityService.getActivitiesByExactDate(exactDate);
-      let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
-      return res.json(response);
-    } catch (err) {
-      console.error('Error al obtener los logs:', err);
-      return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
-    }
-  });
-  
-
 router.get("/:metodo_http", async (req, res) => {
     const metodoHttp = req.params.metodo_http;
     try {
@@ -60,87 +45,99 @@ router.get("/:metodo_http", async (req, res) => {
     }
 });
 
-// Get activities by IP direction
+// Obtener logs por dirección IP
 router.get("/ip/:direccion_ip", async (req, res) => {
     const direccionIp = req.params.direccion_ip;
     try {
-      jwtMiddleware.verifyAndParseToken(req);
-  
-      const activities = await activityService.getActivitiesByIpDirection(direccionIp);
-      let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
-      return res.json(response);
+        jwtMiddleware.verifyAndParseToken(req);
+
+        const activities = await activityService.getActivitiesByIpDirection(direccionIp);
+        let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
+        return res.json(response);
     } catch (err) {
-      console.error('Error al obtener los logs:', err);
-      return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
+        console.error('Error al obtener los logs:', err);
+        return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
     }
-  });
-  
-  // Get activities by URL endpoint
-  router.get("/url/:url_peticion", async (req, res) => {
+});
+
+// Obtener logs por endpoint de URL
+router.get("/url/:url_peticion", async (req, res) => {
     const urlPeticion = req.params.url_peticion;
     try {
-      jwtMiddleware.verifyAndParseToken(req);
-  
-      const activities = await activityService.getActivitiesByUrlEndpoint(urlPeticion);
-      let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
-      return res.json(response);
+        jwtMiddleware.verifyAndParseToken(req);
+
+        const activities = await activityService.getActivitiesByUrlEndpoint(urlPeticion);
+        let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
+        return res.json(response);
     } catch (err) {
-      console.error('Error al obtener los logs:', err);
-      return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
+        console.error('Error al obtener los logs:', err);
+        return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
     }
-  });
-  
-  // Get activities between two dates
-  router.get("/date/:start_date/:end_date", async (req, res) => {
-    const startDate = new Date(req.params.start_date);
-    const endDate = new Date(req.params.end_date);
+});
+
+router.get("/fecha/:fecha_exacta", async (req, res) => {
+    const fechaExacta = new Date(req.params.fecha_exacta);
     try {
-      jwtMiddleware.verifyAndParseToken(req);
-  
-      const activities = await activityService.getActivitiesBetweenDates(startDate, endDate);
-      let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
-      return res.json(response);
+        jwtMiddleware.verifyAndParseToken(req);
+
+        const activities = await activityService.getActivitiesByExactDate(fechaExacta);
+        let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
+        return res.json(response);
     } catch (err) {
-      console.error('Error al obtener los logs:', err);
-      return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
+        console.error('Error al obtener los logs:', err);
+        return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
     }
-  });
-  
-  // Get activities by duration range
-  router.get("/duration/:min_duration/:max_duration", async (req, res) => {
-    const minDuration = parseInt(req.params.min_duration);
-    const maxDuration = parseInt(req.params.max_duration);
+});
+
+// Obtener logs entre dos fechas
+router.get("/fecha/:fecha_inicio/:fecha_fin", async (req, res) => {
+    const fechaInicio = new Date(req.params.fecha_inicio);
+    const fechaFin = new Date(req.params.fecha_fin);
     try {
-      jwtMiddleware.verifyAndParseToken(req);
-  
-      const activities = await activityService.getActivitiesByDurationRange(minDuration, maxDuration);
-      let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
-      return res.json(response);
+        jwtMiddleware.verifyAndParseToken(req);
+
+        const activities = await activityService.getActivitiesBetweenDates(fechaInicio, fechaFin);
+        let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
+        return res.json(response);
     } catch (err) {
-      console.error('Error al obtener los logs:', err);
-      return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
+        console.error('Error al obtener los logs:', err);
+        return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
     }
-  });
-  
-  // Delete activity by ID
-  router.delete("/:id", async (req, res) => {
+});
+
+// Obtener logs por rango de duración
+router.get("/duracion/:duracion_min/:duracion_max", async (req, res) => {
+    const duracionMin = parseInt(req.params.duracion_min);
+    const duracionMax = parseInt(req.params.duracion_max);
+    try {
+        jwtMiddleware.verifyAndParseToken(req);
+
+        const activities = await activityService.getActivitiesByDurationRange(duracionMin, duracionMax);
+        let response = activities ? activities : { success: false, error: 'Logs no encontrados' };
+        return res.json(response);
+    } catch (err) {
+        console.error('Error al obtener los logs:', err);
+        return res.status(500).json({ success: false, error: 'Error al obtener los logs' });
+    }
+});
+
+// Eliminar log por ID
+router.delete("/:id", async (req, res) => {
     const activityId = req.params.id;
     try {
-      jwtMiddleware.verifyAndParseToken(req);
-  
-      const activity = await activityService.getActivityById(activityId);
-      if (!activity) {
-        return res.status(404).json({ success: false, error: 'Actividad no encontrada' });
-      }
-  
-      const deletedActivity = await activityService.deleteActivity(activity);
-      return res.json(deletedActivity);
-    } catch (err) {
-      console.error('Error al eliminar la actividad:', err);
-      return res.status(500).json({ success: false, error: 'Error al eliminar la actividad' });
-    }
-  });
-  
+        jwtMiddleware.verifyAndParseToken(req);
 
+        const activity = await activityService.getActivityById(activityId);
+        if (!activity) {
+            return res.status(404).json({ success: false, error: 'Log no encontrado' });
+        }
+
+        const deletedActivity = await activityService.deleteActivity(activity);
+        return res.json(deletedActivity);
+    } catch (err) {
+        console.error('Error al eliminar el log:', err);
+        return res.status(500).json({ success: false, error: 'Error al eliminar el log' });
+    }
+});
 
 module.exports = router;
