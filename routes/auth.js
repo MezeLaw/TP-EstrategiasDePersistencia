b405af0d777ab2df877ad4045a80bc726d14c1bb
ncoluc <nicolasmcoluccio@gmail.com>
2023-06-26 01:10:35 -0300

    Se crea auth.js con un endpoint para generar el token y un metodo nuevo en el userService.js para usar en la generacion del token

---
+++ routes/auth.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const userService = require('../services/userService');
+const {deleteUser} = require("../services/userService");
+const jwtMiddleware = require('../jwt');
+const bcrypt = require('bcrypt');
+
+const router = express.Router();
+
+router.post('/', async (req, res) => {
+    const { email, password } = req.body;
+    try {
+        var user = userService.getUserByEmail(email);
+        if(user){
+            const passwordMatching = await bcrypt.compare(password, hashedPassword);
+            if (passwordMatching) {
+                const token = jwtMiddleware.generateToken({
+                    id: user.id
+                });
+                res.json(token);
+            } else {
+              console.log('La contraseña es incorrecta');
+              throw new Error('Credenciales incorrectas');
+            }
+        }else{
+            console.error('No se encontro el usuario con email:', email, err);
+            res.status(404).json({ error: 'Usuario no encontrado' });
+        }
+    } catch (err) {
+        console.error('Error al generar token para usuario con email:', email, err);
+        res.status(500).json({ error: 'Error al generar token' });
+    }
+});
+
+//TODO terminar la funcion
+function tokenValidation(token, id) {
+    try {
+        
+    } catch (err) {
+        console.error('Error:', id, err);
+        throw new Error('Error');
+    }
+}
+
+module.exports = router;
