cd3a956fcf861978236096b44189b0792c94f56f
ncoluc <nicolasmcoluccio@gmail.com>
2023-06-28 17:55:34 -0300

    se agrega el reporte con mochawesome

---
+++ mocha-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    reporter: 'mochawesome',
+    reporterOptions: {
+      reportDir: './tests-reports',
+      reportFilename: 'tests-report',
+      quiet: false,
+      json: false
+    }
+  };
