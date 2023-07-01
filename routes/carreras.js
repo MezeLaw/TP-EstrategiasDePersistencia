34f86cf163a3fc60c19bc2414ae4dd5b79fb1387
Meze <mezequielabogado@gmail.com>
2023-07-01 01:41:57 -0300

    feat: refactor de routes introduciendo controllers y agregado de test unitarios (WIP) para controller

--- routes/carreras.js
+++ routes/carreras.js
@@ -1,80 +1,12 @@
 const express = require('express');
-const carreraService = require('../services/carreraService');
-const jwtMiddleware = require("../jwt");
-const adminRol = "ADMIN"
-const permisosInsuficientes = "El usuario no tiene los permisos necesarios para realizar la operacion"
+const { getCarreras, getCarrerasByID, createCarrera, deleteCarrera } = require('../controller/carreraController')
+
+
 const router = express.Router();
 
-// Get carreras
-router.get('/', async (req, res) => {
-    try {
-        const carreras = await carreraService.getCarreras()
-        res.json(carreras);
-    } catch (err) {
-        console.error('Error al obtener las carreras:', err);
-        res.status(500).json({ error: 'Error al obtener las carreras' });
-    }
-});
-
-//Get by id
-router.get("/:id",async (req, res) => {
-    const carreraId = req.params.id;
-    try {
-        const carrera = await carreraService.getCarrera(carreraId)
-        if (carrera) {
-            res.json(carrera);
-        } else {
-            res.status(404).json({ error: 'carrera no encontrada' });
-        }
-    }  catch (err) {
-        console.error('Error al buscar la carrera:', err);
-        res.status(500).json({ error: 'Error al buscar la carrera' });
-    }
-});
-
-//Create carrera
-router.post('/', async (req, res) => {
-    const { name } = req.body;
-    try {
-        const tokenParsed = jwtMiddleware.verifyAndParseToken(req);
-        const rolTokenValidation = await jwtMiddleware.getRolFromToken(tokenParsed);
-        if(rolTokenValidation!== adminRol){
-            console.log(permisosInsuficientes)
-            res.status(401).json({ error: permisosInsuficientes });
-            return
-        }
-        const carrera = await carreraService.createCarrera({ name });
-        res.json(carrera);
-    } catch (err) {
-        console.error('Error al crear la carrera:', err);
-        res.status(500).json({ error: 'Error al crear la carrera' });
-    }
-});
-
-//Delete carrera
-router.delete("/:id",async (req, res) => {
-    const carreraId = req.params.id;
-    try {
-        const tokenParsed = jwtMiddleware.verifyAndParseToken(req);
-        const rolTokenValidation = await jwtMiddleware.getRolFromToken(tokenParsed);
-        if(rolTokenValidation!== adminRol){
-            console.log(permisosInsuficientes)
-            res.status(401).json({ error: permisosInsuficientes });
-            return
-        }
-        const carrera = await carreraService.getCarrera(carreraId)
-        if (carrera) {
-            const deletedCarrera = await carreraService.deleteCarrera(carrera)
-            res.status(200).json({deletedCarrera})
-        } else {
-            res.status(404).json({ error: 'carrera no encontrada' });
-        }
-    }  catch (err) {
-        console.error('Error al buscar la carrera:', err);
-        res.status(500).json({ error: 'Error al buscar la carrera' });
-    }
-});
-
-
+router.get("/", getCarreras)
+router.get("/:id",getCarrerasByID)
+router.post("/", createCarrera)
+router.delete("/:id", deleteCarrera)
 
 module.exports = router;
---
+++ tests/unit/carrera.test.js
@@ -0,0 +1,73 @@
+const chai = require("chai");
+const chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const sinon = require("sinon");
+const proxyquire = require("proxyquire");
+
+
+const tokenRolDecoderStub = sinon.fake.resolves({ rol: "ADMIN" });
+const getCarrerasStub = sinon.stub();
+const getCarrerasStubResult = sinon.stub();
+const carreraStub = sinon.stub();
+const dummyToken = "Token";
+
+
+const { getCarreras, jwt } =
+    proxyquire("./../../controller/carreraController", {
+        './../services/carreraService': {
+            getCarreras: getCarrerasStub
+        },
+        "../jwt": { jwtMiddleware: tokenRolDecoderStub },
+    });
+
+describe("Test carreras controller", () => {
+    beforeEach = () => {
+        getCarrerasStub.resetHistory()
+        getCarrerasStub.resetBehavior()
+        carreraStub.resetHistory()
+        getCarrerasStubResult.resetHistory()
+    };
+
+    it("carrerasController - should return no results for getCarreras", async () => {
+        const req = {
+            headers: {
+                authorization: dummyToken,
+            },
+        };
+
+        const carrerasArray = [];
+        getCarrerasStub.resolves(carrerasArray);
+
+        const jsonMock = sinon.stub();
+        jsonMock.returns(carrerasArray);
+
+        const res = {
+            json: jsonMock,
+        };
+        await getCarreras(req, res);
+        chai.expect(res.json.firstCall.args[0]).to.be.eql([])
+
+    });
+
+    it("carrerasController - should return results for getCarreras", async () => {
+        const req = {
+            headers: {
+                authorization: dummyToken,
+            },
+        };
+        const carrera = { id: "1", name: "Taller de Persistencia",createAt: "30/06/2023", updatedAt: "30/06/2023", deletedAt: null}
+        const carrerasArray = [carrera];
+        getCarrerasStub.resolves(carrerasArray);
+
+        const jsonMock = sinon.stub();
+        jsonMock.returns(carrerasArray);
+
+        const res = {
+            json: jsonMock,
+        };
+        await getCarreras(req, res);
+        chai.expect(res.json.firstCall.args[0]).to.be.eql(carrerasArray)
+
+    });
+
+});
